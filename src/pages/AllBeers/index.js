ec0f33bf1698e6442617a99a8be1a555604d828d
Doug Meurer <douggmeurer@gmail.com>
2022-09-06 10:37:14 -0300

    fix css on all beers

--- src/pages/AllBeers/index.js
+++ src/pages/AllBeers/index.js
@@ -1,65 +1,67 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
-import Navbar from "../../components/NavBar"
-import { useNavigate } from "react-router-dom"
-
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Navbar from "../../components/NavBar";
+import { useNavigate } from "react-router-dom";
 
 function AllBeers() {
+  const [beers, setBeers] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [beers, setBeers] = useState({})
-    const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  useEffect(() => {
+    async function fetchBeers() {
+      try {
+        const response = await axios.get(
+          "https://ih-beers-api2.herokuapp.com/beers"
+        );
+        console.log(response.data);
+        setBeers(response.data);
+        setIsLoading(true);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchBeers();
+  }, []);
 
-    useEffect(() => {
-
-        async function fetchBeers () {
-        
-        try {
-            const response = await axios.get("https://ih-beers-api2.herokuapp.com/beers")
-            console.log(response.data)
-            setBeers(response.data)
-            setIsLoading(true)
-        } catch (error) {
-            console.log(error)
-        } 
-    }   
-    fetchBeers()
-
-    }, [])
-
-    
-
-    return ( 
-
+  return (
     <>
-    <Navbar />
-    <div>
-            <label>All Beers</label>
+      <Navbar />
+      <div>
+        <label>All Beers</label>
 
-            {isLoading && (
+        {isLoading && (
+          <>
+            {beers.map((beer) => {
+              return (
                 <>
-                {beers.map((beer) => {
-                    return (
-                        <div key={beer._id} style={{border: "2px solid grey", marginTop: "2px", marginBottom: "2px"}} onClick={() => navigate(`/beers/${beer._id}`)}>   
-                            <img src={beer.image_url} style={{width: "50px"}} alt="beer"/>
-                            <p>{beer.name}</p>
-                            <p>{beer.tagline}</p>
-                            <p>Created By: {beer.contributed_by}</p>
-                        </div>
-                        
-                        )
-                })}
+                  <div
+                    className="beerContainer"
+                    key={beer._id}
+                    onClick={() => navigate(`/beers/${beer._id}`)}
+                  >
+                    <div className="beerContainerImage">
+                      <img src={beer.image_url} alt="beer" />
+                    </div>
+                    <div className="align-left beerContainerText">
+                      <h3>{beer.name}</h3>
+                      <p className="text-gray">{beer.tagline}</p>
+                      <p>
+                        <small>
+                          <strong>Created By:</strong> {beer.contributed_by}
+                        </small>
+                      </p>
+                    </div>
+                  </div>
                 </>
-            )
-
-            }
-                
-            
-    </div>
+              );
+            })}
+          </>
+        )}
+      </div>
     </>
-    )
+  );
 }
 
-export default AllBeers
+export default AllBeers;
